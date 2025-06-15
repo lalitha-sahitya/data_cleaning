1765ca933297f9cbdd9ed2f7f92cc10bf6bca1f9
lalitha-sahitya <lalithasahitya.palacharla@gmail.com>
2025-06-15 22:18:52 +0530

    done

--- layoffs_dataset_cleaning.sql
+++ layoffs_dataset_cleaning.sql
@@ -97,4 +97,12 @@ UPDATE layoffs_staging2
 SET industry='Low code'
 WHERE industry='';
 
+DELETE
+FROM layoffs_staging2
+WHERE (total_laid_off='' or total_laid_off IS NULL)
+AND
+(percentage_laid_off='' or percentage_laid_off IS NULL);
+
+ALTER TABLE layoffs_staging2
+DROP COLUMN row_num;
 
